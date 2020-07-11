dcf28e97659753222e0e0ff9b798988bd3c7588c
Fidel <fidelmasso@gmail.com>
2020-07-10 21:16:46 -0300

    Implementa 6.2 Lo único que hace es simplificar el cálculo de la ecuación cuadrática y el deteriminante para determinar si el rayo interseca la esfera o no.

--- main.cpp
+++ main.cpp
@@ -9,28 +9,38 @@ double toca_la_esfera(const punto3& centro, double radio, const rayo& r){
 	
 	/*
 	Queremos saber si un rayo toca una esfera.
-	ecuacion del rayo: P = G + ut
+	ecuacion del rayo: P = O + ut
 	ecuacion de esfera: (x - C1)^2  + (y - C2)^2 + (z - C3)^2 = r^2
 
 	si tomamos a (P - C) como un vector que va desde el centro de la esfera hasta su superficie
 	ecuacion vectorial de la esfera:   (P - C) (P - C)   = r^2
 	
 	reemplazamos P por la ecuacion del rayo
-	(G + ut - C) (G + ut - C) = r^2
+	(O + ut - C) (O + ut - C) = r^2
 	resolviendo algebraicamente
-	[u u] t^2 + [2 b (G - C)] t + [(G - C) (G - C)] - r^2 = 0
+	[u u] t^2 + [2 u (O - C)] t + [(O - C) (O - C)] - r^2 = 0
 	esto es una ecuacion cuadratica (que depende de t) con:
-		a =  u u
-		b = 2 b (G - C)
-		c = (G - C) (G - C)
-		discriminante = b b - 4 a c
+	
+	a =  u . u
+	b = 2 u . (O - C) =	2 u . co
+	c = (O - C) . (O - C) = co . co
+	discriminante = b b - 4 a c
+	
 	el rayo toca la esfera si el discriminante de la ecuacion es mayor a 0
-	*/
-	vec3 oc = r.origen() - centro; 
-	auto a = producto_punto(r.direccion(),r.direccion());
-	auto b = 2 * producto_punto(oc,r.direccion());
-	auto c = producto_punto(oc,oc) - radio*radio;
+	
+	Calculo original:
+	auto a = producto_punto(u,u);
+	auto b = 2 * producto_punto(co,u);
+	auto c = producto_punto(co,co) - radio*radio;
 	auto discriminante = b*b - 4*a*c;
+	*/
+	//Calculo simplificado:
+	vec3 co = r.origen() - centro; 
+	vec3 u = r.direccion();
+	auto a = u.longitud_cuadrada();	
+	auto medio_b = producto_punto(co,u);
+	auto c = co.longitud_cuadrada() - radio*radio;
+	auto discriminante = medio_b*medio_b - a*c;
 	
 	if(discriminante<0){
 		return -1.0;
@@ -38,12 +48,13 @@ double toca_la_esfera(const punto3& centro, double radio, const rayo& r){
 	else{
 		//retorna el primer valor de t para el que el rayo toca la esfera
 		//t es siempre un valor positivo en este caso
-		return (-b - sqrt(discriminante)) / (2.0 * a);
+		//return (-b - sqrt(discriminante)) / (2.0 * a); retorno anterior
+		return (-medio_b -sqrt(discriminante)) / a;
 	}
 }
 
 
-//mezcla linealmente blanco y azul dependiendo del alto (coordenada y)
+
 color color_de_rayo(const rayo& r){ 
 	
 	
@@ -51,7 +62,7 @@ color color_de_rayo(const rayo& r){
 	double radio_esfera = 0.5;
 	auto t = toca_la_esfera(centro_esfera, radio_esfera, r);
 	
-	if(t > 0.0){
+	if(t > 0.0){ //el rayo toca la esfera
 		
 		//n es el vector normal de la esfera: n = P - C
 		vec3 n = vector_unitario(r.en(t) - centro_esfera);
@@ -61,6 +72,8 @@ color color_de_rayo(const rayo& r){
 		
 	}
 	
+	//sino, dibujo el fondo
+	//mezclar linealmente blanco y azul dependiendo del alto (coordenada y)
 	vec3 direccion_unitaria = vector_unitario(r.direccion());
 	//en direccion_unitaria, la componente en y se mueve entre -1 y 1
 	//pero necesito que se mueva entre 0 y 1, asi que 
@@ -91,7 +104,7 @@ int main() {
 	//uso (3)
 	auto alto_viewport = 2.0;
 	auto ancho_viewport = relacion_de_aspecto * alto_viewport;
-	auto longitud_focal = 1.0;					//valores de z. Es la distancia de la camara al viewport
+	auto longitud_fcoal = 1.0;					//valores de z. Es la distancia de la camara al viewport
 	
 	auto origen = punto3(0,0,0);				
 	auto horizontal = vec3(ancho_viewport,0,0); //valores de x. Es el ancho del viewport
@@ -99,7 +112,7 @@ int main() {
 	auto esquina_inferior_izquierda = 	origen 
 										- horizontal/2 
 										- vertical/2 
-										- vec3(0,0,longitud_focal);
+										- vec3(0,0,longitud_fcoal);
 	
 	
 	for (int j = alto -1; j >= 0; --j) {
