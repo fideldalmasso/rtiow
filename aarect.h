cb4afcccaefb3acfd5784b37ba0daba7c01ced72
Fidel <fidelmasso@gmail.com>
2021-03-05 23:22:59 -0300

    Documenta cómo funcionan los rectangulos

--- aarect.h
+++ aarect.h
@@ -25,6 +25,24 @@ class rectangulo_xy : public chocable{
 };
 
 bool rectangulo_xy::choca(const rayo& r, double t_min, double t_max, registro_choque& registro) const {
+
+    /*
+    Un rayo puede escribirse como P(t) = A + t*b donde A es un punto y b es la direccion
+    
+    Resolviendo solo para la componente en z queda
+    z = Az + t*bz
+
+    la interseccion con nuestro plano es para z = k entonces
+    k = Az + t*bz
+    t = (k - Az)/bz
+
+    usamos este t para hallar la solucion para las componentes x e y
+    x = Ax + t*bx
+    y = Ay + t*by
+
+    el rayo toca el rectangulo si los x e y hallados pertenecen al rectangulo
+    */
+
     auto t = (k-r.origen().z()) / r.direccion().z();
     if(t < t_min || t > t_max)
         return false;
@@ -34,9 +52,11 @@ bool rectangulo_xy::choca(const rayo& r, double t_min, double t_max, registro_ch
     if(x<x0 || x>x1 || y<y0 || y>y1)
         return false;
 
+    //normaliza las coordenadas para la textura
     registro.u = (x-x0)/(x1-x0);
     registro.v = (y-y0)/(y1-y0);
     registro.t = t;
+
     auto normal_saliente = vec3(0,0,1);
     registro.set_cara_y_normal(r,normal_saliente);
     registro.material_ptr = mp;
